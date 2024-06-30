21d3ac38ae50f54c120e78482e8fe5f313b8a70e
YukiTani067 <166137288+YukiTani067@users.noreply.github.com>
2024-07-01 00:34:13 +0900

    プロジェクト作成

---
+++ src/execution/play.java
@@ -0,0 +1,12 @@
+package execution;
+
+import process.Index;
+
+public class play {
+	
+	public static void main(String[] args) {
+	// Q1：Dogクラスを呼び出して変数に代入し、動物の名前をコンソールに出力してください。
+	Index index = new Index();
+	System.out.println(index.Word1);
+}
+}
---
+++ src/process/Index.java
@@ -0,0 +1,15 @@
+package process;
+
+public class Index {
+	// Q1：フィールドに動物の名前の変数を定義してください。
+	public String Word1;
+	String Word2;
+	String Word3;
+
+	// Q3：Q1で作成した変数に「犬」を代入するコンストラクタを作成してください。
+	public Index() {
+		this.Word1 = "犬";
+		this.Word2 = "犬";
+		this.Word3 = "犬";
+	}
+}
