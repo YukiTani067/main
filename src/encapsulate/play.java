ffb0c2a643cd3f1f69797ed6ecf554c1a4cc10f4
YukiTani067 <166137288+YukiTani067@users.noreply.github.com>
2024-07-04 01:34:55 +0900

    1.23提出

---
+++ src/encapsulate/play.java
@@ -0,0 +1,30 @@
+package encapsulate;
+
+public class play {
+	private String caption = null;
+	
+	public static void main(String[] args) {
+		play word = new play();
+		word.setCaption("""
+				動物名:ライオン
+				体長:2.1m
+				速度:80km/h
+				""");
+		System.out.println(word);
+	}
+	
+	// 変数から値を取得するメソッド
+	public String getCaption() {
+		return caption;
+	}
+
+	// 変数を変更するメソッド
+	public void setCaption(String caption) {
+		this.caption = caption;
+	}
+	
+	// 文字列に変換するメソッド
+	@Override
+	public String toString() {
+		return getCaption();	}
+}
